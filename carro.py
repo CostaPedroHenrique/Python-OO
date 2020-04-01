100476c24f2b05f9ab52dfa7c1ee614d22676265
CostaPedroHenrique <devpedrohenrique@gmail.com>
2020-03-31 23:34:28 -0300

    alteração nos métodos

--- carro.py
+++ carro.py
@@ -1,15 +1,16 @@
 class Carro():
-	def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado):
+	def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado, velocidade):
 		self.cor = cor
 		self.qtd_portas = qtd_portas
 		self.tipo_combustivel = tipo_combustivel
 		self.potencia = potencia
 		self.qtd_combustivel = qtd_combustivel
 		self.is_ligado = is_ligado
+		self.velocidade = velocidade
 
 	
-	def abastecer(self):
-	    self.qtd_combustivel += 20
+	def abastecer(self, qtd_combustivel):
+	    self.qtd_combustivel += qtd_combustivel
 	
 	def ligar(self):
 		if(self.is_ligado):
@@ -24,3 +25,8 @@ class Carro():
 			print('o carro foi desligado')
 		else:
 			print('o carro já está desligado')
+	def acelerar(self, velocidade=10):
+		if self.is_ligado:
+			self.velocidade += velocidade
+		else:
+			print('O carro está desligado')
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import carro
 
-uno_vermelho = carro.Carro('vermelho', 4, 'flex',1.0,0,False)
-uno_vermelho.ligar()
+uno_vermelho = carro.Carro('vermelho', 4, 'flex',1.0,0,False,0)
+
 #print(uno_vermelho.cor)
 
-uno_preto = carro.Carro('preto', 2, 'flex',1.4,0,False)
-uno_preto.desligar()
+uno_preto = carro.Carro('preto', 2, 'flex',1.4,0,False, 0)
+
+uno_preto.abastecer(50)
+print(f"A quantidade de combustivel é: {uno_preto.qtd_combustivel}")
+uno_preto.ligar()
+uno_preto.acelerar(20);
+print('Velocidade: {}'.format(uno_preto.velocidade))
