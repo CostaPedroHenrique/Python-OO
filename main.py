84ab9ccea2e3a824dbc1f45f029c7a02905bd366
CostaPedroHenrique <devpedrohenrique@gmail.com>
2020-03-31 22:57:28 -0300

    Classe carro

---
+++ main.py
@@ -0,0 +1,8 @@
+import carro
+
+uno_vermelho = carro.Carro('vermelho', 4, 'flex',1.0,0,False)
+uno_vermelho.ligar()
+#print(uno_vermelho.cor)
+
+uno_preto = carro.Carro('preto', 2, 'flex',1.4,0,False)
+uno_preto.desligar()
